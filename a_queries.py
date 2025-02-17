755ddb64629cca4f9dd2aca4822a1cf9a82fc1d0
Amelia Spell <amspell@ad.unc.edu>
2025-02-17 12:46:57 -0500

    prep for meeting

---
+++ a_queries.py
@@ -0,0 +1,24 @@
+# # here we will need queries that will be used by the backend to grab data
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="new_password",
+    database="perrypages"
+)
+
+cursor = conn.cursor()
+
+
+cursor.execute("""
+   
+""")
+
+# Commit the changes
+conn.commit()
+print("Executed select el_grade successfully")
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
