01cf373f660476f846287e72fdaff701550bc35c
Amelia Spell <amspell@ad.unc.edu>
2025-02-16 21:12:36 -0500

    setting up sql functionality

---
+++ insertdata.py
@@ -0,0 +1,43 @@
+import mysql.connector
+import pandas as pd
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="new_password",
+    database="perrypages"
+)
+cursor = conn.cursor()
+
+# Insert data into the table
+insert_query = """
+    INSERT INTO books (el_grade, module_num, student_teacher, materials, copy_num, available)
+    VALUES (%s, %s, %s, %s, %s, %s)
+"""
+
+# Read the data from CSV
+df = pd.read_csv('book_data.csv')
+
+# Prepare data for batch insertion
+data = []
+for index, row in df.iterrows():
+    # Append each row as a tuple to the data list
+    data.append((
+        row['el_grade'], 
+        row['module_num'], 
+        row['student_teacher'], 
+        row['materials'], 
+        row['copy_num'], 
+        row['available']
+    ))
+
+# Perform the batch insert
+cursor.executemany(insert_query, data)
+
+# Commit the transaction
+conn.commit()
+
+print("Insert into 'books' successful!")
+
+cursor.close()
+conn.close()
---
+++ setuptable.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="new_password",
+    database="perrypages"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("""
+DROP TABLE IF EXISTS books;
+""")
+
+
+# Creating books table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS books (
+        el_grade VARCHAR(255),
+        module_num INT NOT NULL,
+        student_teacher VARCHAR(255),
+        materials VARCHAR(255),
+        copy_num INT,
+        available BOOL NOT NULL DEFAULT TRUE,
+        PRIMARY KEY (el_grade, module_num, student_teacher, materials, copy_num)
+    )
+""")
+
+# Commit the changes
+conn.commit()
+print("Table 'books' created successfully!")
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
