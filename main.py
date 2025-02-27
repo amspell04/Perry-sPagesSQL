6750aa24e26f635ec8c7b438b2e952baa6b26c61
Amelia Spell <amspell@ad.unc.edu>
2025-02-27 10:30:08 -0500

    working on number increase functionality

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ df = pd.read_csv("book_data.csv")
 
 data = df[["el_grade", "module_num", "student_teacher", "materials", "copy_num", "available"]].values.tolist()
 
+checkoutlist = ["checkout1", "checkout2", "checkout3", "checkout3", "checkout3", "checkout3"]
+
 app = FastAPI()
 
 app.add_middleware(
@@ -18,3 +20,14 @@ app.add_middleware(
 @app.get("/getdata")
 async def get_data():
     return data
+
+@app.get("/getcheckoutlist")
+async def get_checkout():
+    return checkoutlist
+
+
+
+@app.get("/increasecount{count}")
+async def increasecount(currcount: int):
+    currcount += 1
+    return currcount
